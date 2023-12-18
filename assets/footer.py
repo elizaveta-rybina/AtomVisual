39674e84d918abf8babc257501abe3d840ac1f52
elizaveta_rybina <liza.rybina@inbox.ru>
2023-12-18 17:48:17 +0300

    init

---
+++ assets/footer.py
@@ -0,0 +1,36 @@
+import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+
+GITHUB_LISA = 'https://github.com/elizaveta-rybina'
+CONTACT_ICON_WIDTH = 25
+
+footer = dmc.Grid(
+    [
+        dmc.Col(
+            [
+                dmc.Footer(
+                    height=30,
+                    fixed=True,
+                    className='footer-container',
+                    style={
+                        'backgroundColor': 'rgba(0,0,0,0)',
+                    },
+                    mb=5,
+                    withBorder=False,
+                    children=[
+                        dmc.Group(
+                            children=[
+                                dmc.Anchor(
+                                    children=[DashIconify(
+                                        icon='mdi:github', width=CONTACT_ICON_WIDTH)
+                                    ],
+                                    href=GITHUB
+                                )
+                            ], position='center'
+                        )
+                    ]
+                )
+            ], span=12
+        )
+    ]
+)
